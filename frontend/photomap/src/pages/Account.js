a0812036c02400a120207f77ddf5622ffcb452d5
katya <katyamit5@gmail.com>
2024-03-15 19:03:08 +0300

    simple route

--- frontend/photomap/src/pages/Account.js
+++ frontend/photomap/src/pages/Account.js
@@ -1,13 +1,23 @@
-// Account.js
 import React from 'react';
+import { YMaps, Map, Placemark, RoutePanel } from '@pbe/react-yandex-maps';
 
-const Account = () => {
-  return (
-    <div>
-      <h2>Account</h2>
-      {/* Здесь будет содержимое страницы Account */}
-    </div>
-  );
-};
+class Account extends React.Component {
+  render() {
+    const mapStyles = {
+      width: '90vw', 
+      height: '90vh', 
+    };
+
+    return (
+      <YMaps query={{apikey: '7c779eb6-845b-4c78-a612-a2397737206e'}}>
+        <div style={mapStyles}>
+          <Map defaultState={{ center: [55.751574, 37.573856], zoom: 9 }} style={mapStyles}>
+            <RoutePanel />
+          </Map>
+        </div>
+      </YMaps>
+    );
+  }
+}
 
 export default Account;
