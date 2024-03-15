2d9a779352fc611071c04ad622c75412879dd378
katya <katyamit5@gmail.com>
2024-03-15 20:29:02 +0300

    add multirouting

--- frontend/photomap/src/index.js
+++ frontend/photomap/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -31,11 +31,6 @@ const firebaseConfig = {
   measurementId: "G-9GCFYP8JRY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- frontend/photomap/src/pages/Account.js
+++ frontend/photomap/src/pages/Account.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { YMaps, Map, Placemark, RoutePanel } from '@pbe/react-yandex-maps';
+import React, { useRef } from 'react';
+import { YMaps, Map } from 'react-yandex-maps';
 
-class Account extends React.Component {
-  render() {
-    const mapStyles = {
-      width: '90vw', 
-      height: '90vh', 
-    };
+const Account = () => {
+  const map = useRef(null);
+  const multiRouteRef = useRef(null);
 
-    return (
-      <YMaps query={{apikey: '7c779eb6-845b-4c78-a612-a2397737206e'}}>
-        <div style={mapStyles}>
-          <Map defaultState={{ center: [55.751574, 37.573856], zoom: 9 }} style={mapStyles}>
-            <RoutePanel />
-          </Map>
-        </div>
-      </YMaps>
+  const mapState = {
+    center: [55.739625, 37.5412],
+    zoom: 12
+  };
+
+  const addRoute = (ymaps) => {
+    const pointA = [55.749, 37.524];
+    const pointB = "Москва, Красная площадь";
+    const pointC = "Москва, Павелецкий вокзал";
+    const pointD = "Москва, Таганская";
+
+    if (multiRouteRef.current) {
+      map.current.geoObjects.remove(multiRouteRef.current);
+    }
+
+    const multiRoute = new ymaps.multiRouter.MultiRoute(
+      {
+        referencePoints: [pointA, pointB, pointC, pointD],
+        params: {
+          routingMode: "pedestrian"
+        }
+      },
+      {
+        boundsAutoApply: true
+      }
     );
-  }
+
+    map.current.geoObjects.add(multiRoute);
+    multiRouteRef.current = multiRoute;
+  };
+
+  return (
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+      <YMaps query={{ apikey: '7c779eb6-845b-4c78-a612-a2397737206e' }}>
+        <Map
+          state={mapState}
+          instanceRef={map}
+          modules={['multiRouter.MultiRoute']}
+          onLoad={addRoute}
+          style={{ width: '100%', height: '100%' }}
+        />
+      </YMaps>
+    </div>
+  );
 }
 
 export default Account;
